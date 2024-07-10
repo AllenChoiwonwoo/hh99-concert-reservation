7c78ff1c625324b23b01ba84d62d23fef0043fb7
allen-adop <allen@adop.cc>
2024-07-10 20:52:56 +0900

    Feat. 콘서트 도메인

--- src/main/java/com/hh99/hh5concertreservation/waiting/presentation/TokenValidationAspect.java
+++ src/main/java/com/hh99/hh5concertreservation/waiting/presentation/TokenValidationAspect.java
@@ -16,13 +16,13 @@ public class TokenValidationAspect {
     private final HttpServletRequest request;
     private final QueueService queueService;
 
-    @Pointcut("within(com.hh99.hh5concertreservation.concert.presentation.ConcertController.*)")
-    public void validateToken() {
-        String token = request.getHeader("Token");
-        Long userId = Long.valueOf(request.getHeader("UserId"));
-
-        if (token == null || !queueService.isValidate(userId, token)) {
-            throw new IllegalArgumentException("Invalid or missing token");
-        }
-    }
+//    @Pointcut("within(com.hh99.hh5concertreservation.concert.presentation.ConcertController.*)")
+//    public void validateToken() {
+//        String token = request.getHeader("Token");
+//        Long userId = Long.valueOf(request.getHeader("UserId"));
+//
+//        if (token == null || !queueService.isValidate(userId, token)) {
+//            throw new IllegalArgumentException("Invalid or missing token");
+//        }
+//    }
 }
